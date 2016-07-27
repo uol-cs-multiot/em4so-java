2bb487a9c5c4b873d48cd676570112d6fea14a62
Ludovic <ludovic.clarissou@telecom-st-etienne.fr>
2016-07-27 19:32:29 +0100

    display message Telegram in log file

--- examples/stv/src/main/java/capabilities/AlertManagement.java
+++ examples/stv/src/main/java/capabilities/AlertManagement.java
@@ -1,21 +1,65 @@
 package capabilities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.mp.em4so.model.actuating.Capability;
 import org.mp.em4so.model.common.Element;
 
-public class AlertManagement extends Capability {
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
 
-	
-	
-	public void notifyUser(String content, Element user){
-		HashMap<String,String> userAttributes =  user.getAttributes();
-		LOG.info("message:"+content+userAttributes.get("message")+" displayed to user: "+userAttributes.get("name"));
-		System.out.println("message:"+content+userAttributes.get("message")+" displayed to user: "+userAttributes.get("name"));
+public class AlertManagement extends Capability
+{
+
+	// private TelegramBot BOT;
+	private int lastUpdateId;
+
+	/** Telegram */
+	protected final TelegramBot BOT = TelegramBotAdapter.build("262473126:AAGF5zvWXpssuP-SkedQkGUdbdn16A-sRiQ");
+
+	public enum Action
+	{
+		REGISTER, DEREGISTER, INTERACTION, SEND_MESSAGE
 	}
-	
-	public void listenToTelegram(){
-		LOG.info("Listening to Telegram.....");
+
+	public List<Long> users;
+
+	public AlertManagement()
+	{
+		super();
+		lastUpdateId = 662306126 + 1;
+		LOG.trace("In AlertManagement constructor");
+		users = new ArrayList<Long>();
+		users.add(new Long(224322144));
+	}
+
+	public void notifyUser(String content, Element user)
+	{
+		HashMap<String, String> userAttributes = user.getAttributes();
+		LOG.info("message:" + content + userAttributes.get("message") + " displayed to user: "
+				+ userAttributes.get("name"));
+		System.out.println("message:" + content + userAttributes.get("message") + " displayed to user: "
+				+ userAttributes.get("name"));
+	}
+
+	public void listenToTelegram()
+	{
+		LOG.trace("In listenToTelegram method");
+		List<Update> updates = BOT.execute(new GetUpdates().limit(0).offset((int) lastUpdateId).timeout(0)).updates();
+		LOG.trace("lastUpdateId : " + lastUpdateId);
+		LOG.trace("taille liste update : " + updates.size());
+		lastUpdateId = updates.get(updates.size() - 1).updateId() + 1;
+		for (Update update : updates)
+		{
+			LOG.trace("$$" + update.message().chat().id() + " send a message : '" + update.message().text()
+					+ "', update id : " + update.updateId());
+			boolean hasUpdate = (update == null ? true : false);
+			// TODO : Dispatcher
+		}
+
 	}
 }
