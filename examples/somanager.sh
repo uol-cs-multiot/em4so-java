3b2728b4409537a98c284295975baab79adca9be
mperhez <meph1@le.ac.uk>
2016-07-27 11:02:05 +0100

    Fixed paths in somanager script

--- examples/somanager.sh
+++ examples/somanager.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-SO_PATH=/home/mp/workspace/git/em4so-java
+#This script will work if you pass as arguments: (1) name of the SO to run (2) name of the linux user for home directory. The folder git project root should be in the SO_PATH.
+SO_PATH=/home/$2/workspace/git/em4so-java
 JAR_SOM="$SO_PATH/somanager/build/libs/somanager-all-0.1.jar"
 JAR_SO="$SO_PATH/examples/$1/build/libs/$1-0.1.jar"
 java -cp $JAR_SO:$JAR_SOM org.mp.em4so.launcher.SOMLauncher $SO_PATH/examples/$1/$1.yaml
