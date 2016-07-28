b83cfbec81ecae8150a0d67316fa7a8f14712c19
Ludovic <ludovic.clarissou@telecom-st-etienne.fr>
2016-07-28 10:09:49 +0100

    Add dispatcher Skeleton

--- examples/stv/src/main/java/capabilities/AlertManagement.java
+++ examples/stv/src/main/java/capabilities/AlertManagement.java
@@ -19,13 +19,19 @@ public class AlertManagement extends Capability
 	private int lastUpdateId;
 
 	/** Telegram */
-	protected final TelegramBot BOT = TelegramBotAdapter.build("262473126:AAGF5zvWXpssuP-SkedQkGUdbdn16A-sRiQ");
+	protected final TelegramBot BOT = TelegramBotAdapter.build("<TOKEN>");
+
+	public enum Interaction
+	{
+		TEMPEARATURE, MOTION;
+	}
 
 	public enum Action
 	{
-		REGISTER, DEREGISTER, INTERACTION, SEND_MESSAGE
+		REGISTER, DEREGISTER, SEND_MESSAGE, ERROR, INTERACTION
 	}
 
+	private Action action;
 	public List<Long> users;
 
 	public AlertManagement()
@@ -48,18 +54,45 @@ public class AlertManagement extends Capability
 
 	public void listenToTelegram()
 	{
-		LOG.trace("In listenToTelegram method");
-		List<Update> updates = BOT.execute(new GetUpdates().limit(0).offset((int) lastUpdateId).timeout(0)).updates();
-		LOG.trace("lastUpdateId : " + lastUpdateId);
+
+		List<Update> updates = BOT.execute(new GetUpdates().limit(100).offset((int) lastUpdateId).timeout(0)).updates();
 		LOG.trace("taille liste update : " + updates.size());
 		lastUpdateId = updates.get(updates.size() - 1).updateId() + 1;
+		LOG.trace("LASTUDPATEID " + lastUpdateId);
 		for (Update update : updates)
 		{
-			LOG.trace("$$" + update.message().chat().id() + " send a message : '" + update.message().text()
+			LOG.trace(update.message().chat().id() + " send a new message : '" + update.message().text()
 					+ "', update id : " + update.updateId());
-			boolean hasUpdate = (update == null ? true : false);
-			// TODO : Dispatcher
-		}
+			System.out.println(update.message().chat().id() + " send a new message : '" + update.message().text()
+					+ "', update id : " + update.updateId());
 
+			// Dispatcher
+			switch (action = textToAction(update.message().text()))
+			{
+				case DEREGISTER:
+					System.out.println("Deregister");
+					break;
+				case REGISTER:
+					System.out.println("Register");
+					break;
+				case INTERACTION:
+					System.out.println("Interaction");
+					break;
+				default:
+					System.out.println("Error");
+			}
+		}
 	}
+
+	private Action textToAction(String message)
+	{
+		if (message.toLowerCase().contains("deregister"))
+			return Action.DEREGISTER;
+		else if (message.toLowerCase().contains("register"))
+			return Action.REGISTER;
+		else if (message.toLowerCase().contains("interaction"))
+			return Action.INTERACTION;
+		return Action.ERROR;
+	}
+
 }
